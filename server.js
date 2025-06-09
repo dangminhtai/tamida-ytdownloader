f0f3faab0e8dad820a5605d1b3ff5236114cceb0
dangminhtai <dmt826321@gmail.com>
2025-06-09 08:42:31 +0700

    fix port

--- server.js
+++ server.js
@@ -216,6 +216,7 @@ app.post("/download", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("🚀 Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
